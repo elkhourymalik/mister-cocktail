670c54f560a79802120266bb149866f9e79b82a4
malik el khoury <elkhourymalik@gmail.com>
2016-02-12 13:03:47 +0100

    debuging

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -36,7 +36,7 @@ class CocktailsController < ApplicationController
 
   private
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo, :photo_cache)
+    params.require(:cocktail).permit(:name, :photo)
   end
 
   def set_cocktail
